293b0adb1d144ed023a7d597cbe2df8546c72072
Greg Sugiyama <gjsugiyama@gmail.com>
2017-11-15 19:13:17 -0800

    Added socket.io connection to react App.js
    Verified server state transfer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {slapState: {}};
+  };
+
+  //RECIVES STATE DATA 
+  componentDidMount() {
+    this.socket = io('localhost:3001');
+    this.socket.on('state', (slapState) => {
+      this.setState({slapState});
+    })
+  };
+
   render() {
     return (
       <div className="App">
