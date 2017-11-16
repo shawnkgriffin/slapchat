1809c3e0bb34604a17e437b0daff6148f2595a19
Greg Sugiyama <gjsugiyama@gmail.com>
2017-11-15 17:24:42 -0800

    added disconnect socket and index.html

--- server/server.js
+++ server/server.js
@@ -3,11 +3,23 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
-users = [];
-connections = [];
+let connections = [];
 
 server.listen(process.env.PORT || 3001)
 
+//Index HTML is for debugging
 app.get('/', (req, res) =>{
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
+});
+
+//Socket on connect
+io.sockets.on('connection', (socket) => {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+    //Disconnect
+    socket.on('disconnect', (data) => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected %s sockets connnected', connections.length);
+    });
 });
