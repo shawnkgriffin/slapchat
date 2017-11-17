373b69ac7e425bdf49282f3ec75d96ab28b13bc7
SpinnyFinny <finn.hiltner@hotmail.com>
2017-11-17 14:56:54 -0800

    installed  react-google-maps, rendered intial map

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+    <GoogleMap
+      defaultZoom={13}
+      defaultCenter={{ lat: 50.093284, lng: -122.934940 }}
+    >
+      {props.isMarkerShown && <Marker position={{ lat: 50.059167, lng: -122.956944 }} />}
+    </GoogleMap>
+  )
+)
 class Map extends Component {
   render() {
     return (
       <div className="map-container">
+        <MyMapComponent
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
     )
   }
