d404e91fd502c98842a6e159c55091d719efc0cc
SpinnyFinny <finn.hiltner@hotmail.com>
2017-11-19 19:09:02 -0800

    Made user info it's own component, started sytling section heads in sidebar

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
 import Users from "./Users.js";
 import Channels from "./Channels.js";
+import UserInfo from "./UserInfo.js";
 
 class SideBar extends Component {
   render() {
     return (
       <div className="side-bar">
-        <div className="user-info">
-          <p>User info content</p>
-        </div>
+        <UserInfo />
         <div className="side-bar-nav">
           <section className="channels">
-            <span>Channels</span>
+            <span className="side-bar-section-header">Channels</span>
             <Channels channels={this.props.channels} />
           </section>
           <section className="direct-messsages">
-            <span>Direct Messages</span>
+            <span className="side-bar-section-header">Direct Messages</span>
             <Users users={this.props.users} />
           </section>
         </div>
