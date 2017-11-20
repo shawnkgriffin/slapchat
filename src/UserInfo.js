208a4e10064b5eecaf7e232816433cab48fcbbb4
SpinnyFinny <finn.hiltner@hotmail.com>
2017-11-19 20:21:13 -0800

    minimal styling of userinfo for development

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -4,7 +4,7 @@ class UserInfo extends Component {
   render() {
     return (
       <section className="user-info">
-        <div>DeBarge</div>
+        <div>DeBarge:</div>
         <div>Patrol</div>
       </section>
     );
