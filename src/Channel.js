844065c9abe961b5e27cd4f8a4b70233674674ed
Shawn Griffin <shawn@shawngriffin.com>
2017-11-21 08:15:34 -0800

    Getting callback with channel info

--- src/Channel.js
+++ src/Channel.js
@@ -3,7 +3,7 @@ import React from "react";
 function Channel({ channel, onChannelCallback }) {
   return (
     <li
-      onClick={onChannelCallback}
+      onClick={channelState => onChannelCallback(channel, channelState)}
       className="channel-channelname list-group-item"
     >
       {channel.name}
