108e4af27aaba7be541be40b4b522ea1dfa861c0
SpinnyFinny <finn.hiltner@hotmail.com>
2017-11-22 19:23:45 -0800

    added database usl to knexfile

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,9 @@ module.exports = {
   production: {
     client: "postgresql",
     connection: {
-      database: process.env.DB_DATABASE,
+      // database: process.env.DB_DATABASE,
+      database:
+        "postgres://obycioxcejlrbh:065f7a8d7ccd0f7fd44e42c4b56b2473195dd04c7a6c2bb83e33f24fdddc78ff@ec2-54-221-207-138.compute-1.amazonaws.com:5432/db5d1nana0t8v6",
       user: process.env.DB_USER,
       password: process.env.DB_PASS
     },
